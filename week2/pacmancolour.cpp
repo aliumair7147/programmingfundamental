0fd907810cd5eb2013a93e1f0b0e8db3c8befc52
aliumair7147 <auj7147@gmail.com>
2026-02-22 23:02:59 +0500

    first week tasks

---
+++ week2/pacmancolour.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    system("colour17");
+    cout << "      . . : : -- - : : . .      " << endl;
+    cout << "   . . . . . . . . . . . . . .  " << endl;
+    cout << " . . . . . . . . . . . . . . .  " << endl;
+    cout << ". . . . . . . . . . . . . . . . " << endl;
+    cout << ". . . . . . . . . . . . . . - - . " << endl;
+    cout << ". . . . . . . . . . . . - - : . " << endl;
+    cout << ": - - - - - - - - - - : : .     " << endl;
+    cout << ". . . . . . . . . . - - : .     " << endl;
+    cout << ". . . . . . . . . . . . . - - : . " << endl;
+    cout << ": - - - - - - - - - - - - - - : ." << endl;
+    cout << " . . . . . . . . . . . . . . . : " << endl;
+    cout << "      . : - - - - - - - : .     " << endl;
+
+    return 0;
+}
